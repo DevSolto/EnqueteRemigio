6d5543be1587dff6350f3cb2c0a479c5f778b911
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2024-04-11 21:49:51 +0000

    V 1.0

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 from time import sleep
 
-class Enquete:
-    def __init__(self) -> None:
-        self.link = "https://eleicao.top/?page_id=569"
-        self.driver = webdriver.Chrome(executable_path="\\chromedriver.exe")
-        self.driver.maximize_window()
+servico = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=servico)
 
-    def abrirSite(self):
-        sleep(1)
-        self.driver.get(self.link)
-        sleep(10)
+for i in range(0,20):
 
-    
-enquete = Enquete()
 
-enquete.abrirSite()
+    navegador.get("https://eleicao.top/?page_id=569")
+
+    sleep(1)
+
+    navegador.find_element(by=By.CLASS_NAME, value="totalpoll-question-choices-item-container").click()
+
+    navegador.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div/main/div/div/section[2]/div[2]/div/div/div[2]/div/div/div/div/form/div[4]/button[2]").click()
+    sleep(5)
+
+    navegador.delete_all_cookies() 
